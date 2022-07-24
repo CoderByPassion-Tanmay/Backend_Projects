bbc9e53b1054161206d76615032f0025f6f06d23
tanmay <coderbypassion702@gmaiil.com>
2022-07-25 00:26:23 +0530

    added for making foodApp

--- Project_1/server.js
+++ Project_1/server.js
@@ -4,17 +4,7 @@ const app = express();
 //     res.end("hello be")
 // })
 app.use(express.json());
-app.get("/square/:num1", function (req,res) {
-    
-    let no = req.params.num1;
-    let result = no * no ;
-    res.end("result  "+ result);
-})
-app.post("/sayConnect" , function(req ,result)
-{
-    console.log("data",req.body);
-    res.end("post wala server")
-})
+
 app.listen(3000 , function ()
 {
     console.log("Sevcbbhxcvhrvercvhn error 202");
